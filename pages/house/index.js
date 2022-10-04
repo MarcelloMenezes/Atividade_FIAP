e8fb4d84d89597cd98ae0e4ed9df9712898d38cf
Marcello Menezes <marcelinho.augusto199840@gmail.com>
2022-10-04 00:22:38 -0300

    feat: adicionando mapa e modais para música e portão

--- pages/house/index.js
+++ pages/house/index.js
@@ -1,31 +1,21 @@
+let modal = document.querySelector('.card-modal')
+
 function possibilidadesUsuario(item) {
     const alert = document.querySelector('.alert');
     const textModal = document.querySelector('.textModal');
 
     switch (item) {
-        case 'music': ;
-            textModal.innerHTML = `Tocando na Alexa`
-            break;
         case 'ar':
-
             textModal.innerHTML = `Ar-condicionado ligado`
             break;
-
         case 'tv':
-
             textModal.innerHTML = `TV ligada`
             break;
         case 'coffee':
-
             textModal.innerHTML = `Cafeteira ligada`
             break;
         case 'light':
-
             textModal.innerHTML = `Luz da garagem acessa`
-            break;
-        case 'open':
-
-            textModal.innerHTML = `Portão aberto`
             break;
         default:
             break;
@@ -35,6 +25,33 @@ function possibilidadesUsuario(item) {
     setTimeout(() => {
         alert.classList.add('hidden');
     }, 5000)
+}
 
+function abrirModal(item) {
 
+    let modalTitle = document.querySelector('.modal-title')
+    let modalBody = document.querySelector('.modal-body')
+    let btnModal = document.querySelector('.btn-modal')
+
+    modal.classList.remove('modal')
+    modalBody.classList.remove('hidden')
+
+    switch (item) {
+        case 'music':
+            modalTitle.innerHTML = `Digite nome da música ou do artista:`
+            btnModal.innerHTML = `Salvar`
+            break;
+        case 'open':
+            modalTitle.innerHTML = `200 metros da casa, deseja abrir o portão?`
+            modalBody.classList.add('hidden')
+            btnModal.innerHTML = `Sim`
+            break;
+
+        default:
+            break;
+    }
+}
+
+function fecharModal() {
+    modal.classList.add('modal')
 }
